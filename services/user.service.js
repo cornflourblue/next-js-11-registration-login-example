8692982715b16e6030e069b4c24c9e93e088e582
Jason Watmore <cornflourblue@hotmail.com>
2021-08-19 14:48:03 +1000

    added rxjs notification on user update

--- services/user.service.js
+++ services/user.service.js
@@ -51,7 +51,19 @@ function getById(id) {
 }
 
 function update(id, params) {
-    return fetchWrapper.put(`${baseUrl}/${id}`, params);
+    return fetchWrapper.put(`${baseUrl}/${id}`, params)
+        .then(x => {
+            // update stored user if the logged in user updated their own record
+            if (id === userSubject.value.id) {
+                // update local storage
+                const user = { ...userSubject.value, ...params };
+                localStorage.setItem('user', JSON.stringify(user));
+
+                // publish updated user to subscribers
+                userSubject.next(user);
+            }
+            return x;
+        });
 }
 
 // prefixed with underscored because delete is a reserved word in javascript
